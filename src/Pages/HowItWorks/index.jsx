4fb86af50d50fca77d5c9e38bbdee12db59111cb
dove <codegeekdove@gmail.com>
2023-10-24 02:32:17 +0800

    fixed some link issues and add how it works components

---
+++ src/Pages/HowItWorks/index.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../../Components/NavBar'
+
+function HowItWorks() {
+  return (
+    <div>
+      <NavBar />
+
+      <h1>How it works</h1>
+    </div>
+  )
+}
+
+export default HowItWorks
