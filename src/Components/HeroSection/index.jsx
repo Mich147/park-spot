254115b4cdc2d8e9b6b9debbe6b03e5f6afdc420
dove <codegeekdove@gmail.com>
2023-11-27 17:24:13 +0800

    test commit

--- src/Components/HeroSection/index.jsx
+++ src/Components/HeroSection/index.jsx
@@ -1,20 +1,18 @@
-import ToggleButton from "../ToggleButton";
-import BookParkSection from "../BookParkSection";
+import BookParkSection from '../BookParkSection'
+import ToggleButton from '../ToggleButton'
 
 function HeroSection() {
   return (
     <div className="hero-image">
       <h1>
-        Easy parking at the <br /> tip of your fingers
+        Easy parking at the <br /> tip of your fingers doigsdgsdgsdgposdjgpsdjgpsjgpsdpgsdgsd
       </h1>
       <h3>Book your car park now!</h3>
-      <h4 className="mt-xl-5  ">
-        Select the type of car park you are looking for:
-      </h4>
+      <h4 className="mt-xl-5  ">Select the type of car park you are looking for:</h4>
       <ToggleButton />
       <BookParkSection />
     </div>
-  );
+  )
 }
 
-export default HeroSection;
+export default HeroSection
