5ab26580b2ff94281b18caf12f4647b2d52407be
dove <codegeekdove@gmail.com>
2023-11-27 17:25:00 +0800

    test commit

--- src/Components/HeroSection/index.jsx
+++ src/Components/HeroSection/index.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div className="hero-image">
       <h1>
-        Easy parking at the <br /> tip of your fingers doigsdgsdgsdgposdjgpsdjgpsjgpsdpgsdgsd
+        Easy parking at the <br /> tip of your fingers
       </h1>
       <h3>Book your car park now!</h3>
       <h4 className="mt-xl-5  ">Select the type of car park you are looking for:</h4>
