075ba0a885f6bd487bb3014f4faa737389e0ef01
dove <codegeekdove@gmail.com>
2023-11-28 19:37:37 +0800

    refresh app by deleting all files

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Router from './Router'
+
+import App from './App'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 )
