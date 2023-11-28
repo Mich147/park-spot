6e7e628347518c361669e911e6423e1cdc2d7744
dove <codegeekdove@gmail.com>
2023-11-28 19:30:03 +0800

    removed navbar  for the mean time

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
 // import Footer from './components/Footer'
-import Navigation from './components/Navigation'
+// import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div>
-      <Navigation />
+      {/* <Navigation /> */}
       <main>
         <Outlet />
       </main>
