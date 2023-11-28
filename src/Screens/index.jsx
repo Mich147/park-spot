b616ace07e627955403f5d0d727e21768a8b2739
dove <codegeekdove@gmail.com>
2023-11-28 20:59:35 +0800

    added route for contact  page

--- src/Screens/index.jsx
+++ src/Screens/index.jsx
@@ -1,3 +1,4 @@
+export { default as ContactScreen } from './ContactScreen'
 export { default as ErrorPage } from './ErrorScreen'
 export { default as HomeScreen } from './HomeScreen'
 export { default as HowItWorksScreen } from './HowItWorksScreen'
