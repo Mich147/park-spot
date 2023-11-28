6cf94d9b9d4f3e6f12035e7c1de938e994c9ce79
dove <codegeekdove@gmail.com>
2023-11-28 20:37:12 +0800

    added overview section

--- src/Screens/HomeScreen/index.jsx
+++ src/Screens/HomeScreen/index.jsx
@@ -1,10 +1,12 @@
 import Hero from '../../components/Hero'
+import HomeOverviewSection from '../../components/HomeOverviewSection'
 import HomeStatsSection from '../../components/HomeStatsSection'
 function HomeScreen() {
   return (
     <>
       <Hero />
       <HomeStatsSection />
+      <HomeOverviewSection />
     </>
   )
 }
