32f49e6ba6a20f54aa12ffcd17f5a27694663b4a
dove <codegeekdove@gmail.com>
2023-12-02 21:15:41 +0800

    finished the sign up form validation feature

--- src/components/UserSignUp/index.jsx
+++ src/components/UserSignUp/index.jsx
@@ -1,33 +1,76 @@
 import { Button, Col, Container, Form, Row } from 'react-bootstrap'
 
+import { useEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
 
 function UserSignUp() {
+  const [user, setUser] = useState({})
+  const [error, setError] = useState({})
+
+  const userRef = useRef(null)
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const message = {}
+
+    if (!user.username) {
+      message.username = 'Please provide username'
+    }
+
+    if (!user.email) {
+      message.email = 'Please provide email'
+    }
+    if (!user.password) {
+      message.password = 'Please provide password'
+    }
+
+    if (!user.confirmPassword) {
+      message.confirmPassword = 'Please confirm password'
+    } else if (user.password !== user.confirmPassword) {
+      message.confirmPassword = 'Password did not matched'
+    }
+
+    setError(message)
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target
+    setUser((prev) => ({ ...prev, [name]: value }))
+  }
   return (
     <section className="signUp py-5">
       <Container>
         <Row className={` mx-auto justify-content-center`}>
           <Col lg={8}>
             <h1 className="text-center mb-4">Sign Up As User</h1>
-            <Form className={`${styles.formBody} py-4 px-3`}>
-              <Form.Group className="mb-3">
+            <Form className={`${styles.formBody} py-4 px-3`} onSubmit={handleSubmit}>
+              <Form.Group className="">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" />
+                <Form.Control type="text" ref={userRef} name="username" onChange={handleChange} />
+                <p style={{ color: 'crimson' }}> {error.username && error.username}</p>
+                {/* {error.username && <p className=''>{error.username}</p>} */}
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" autoComplete="off" />
+                <Form.Control type="email" name="email" autoComplete="off" onChange={handleChange} />
+                <p style={{ color: 'crimson' }}> {error.email && error.email}</p>
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" autoComplete="off" />
+                <Form.Control type="password" name="password" autoComplete="off" onChange={handleChange} />
+                <p style={{ color: 'crimson' }}> {error.password && error.password}</p>
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" />
+                <Form.Control type="password" name="confirmPassword" onChange={handleChange} />
+                <p style={{ color: 'crimson' }}> {error.confirmPassword && error.confirmPassword}</p>
               </Form.Group>
 
               <Button variant="primary" type="submit">
