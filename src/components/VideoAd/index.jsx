6c9450fff39e5a1e4b1591dceef58962ca5ec37f
dove <codegeekdove@gmail.com>
2023-12-08 20:02:34 +0800

    fixed help and how it works page responsiveness

--- src/components/VideoAd/index.jsx
+++ src/components/VideoAd/index.jsx
@@ -1,12 +1,18 @@
 import { Container, Row } from 'react-bootstrap'
 
+import styles from './style.module.css'
+
 function VideoAd() {
   return (
     <section className="videoad">
       <Container>
         <Row className="py-5">
-          <div className="container text-center mx-auto">
-            <iframe width="1280" height="720" src="https://www.youtube.com/embed/zyD94g1A2q8?autoplay=1" frameBorder="0"></iframe>
+          <div className="container text-center mx-auto img-fluid">
+            <iframe
+              className={styles.vidad}
+              src="https://www.youtube.com/embed/zyD94g1A2q8?autoplay=1"
+              frameBorder="0"
+            ></iframe>
           </div>
         </Row>
       </Container>
