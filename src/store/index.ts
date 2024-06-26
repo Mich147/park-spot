2ae38ea955cdd813995dad0d8d314ab9a7826820
dove <codegeekdove@gmail.com>
2024-06-26 10:24:32 +0800

    feat: delete rootreducer file

--- src/store/index.ts
+++ src/store/index.ts
@@ -16,13 +16,3 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export default store
-
-// import { configureStore } from '@reduxjs/toolkit'
-// import rootReducer from './rootReducer'
-
-// const store = configureStore({
-//   reducer: rootReducer,
-// })
-
-// export type AppDispatch = typeof store.dispatch
-// export default store
